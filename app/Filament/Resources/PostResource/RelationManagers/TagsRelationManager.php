9714b5cde32a78d14fa0f77db3152263b82a5973
Ali <qali372@gmail.com>
2024-02-19 07:46:31 +0200

    fix : add form and table fields to TagsRelationManager.php to show the related tags to a specific post

--- app/Filament/Resources/PostResource/RelationManagers/TagsRelationManager.php
+++ app/Filament/Resources/PostResource/RelationManagers/TagsRelationManager.php
@@ -2,13 +2,18 @@
 
 namespace App\Filament\Resources\PostResource\RelationManagers;
 
+use Closure;
 use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
 
 class TagsRelationManager extends RelationManager
 {
@@ -20,9 +25,13 @@ class TagsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
+                Card::make()->schema([
+                    TextInput::make('name')->reactive()
+                        ->afterStateUpdated(function (Closure $set,$state){
+                            $set('slug', Str::slug($state));
+                        })->required(),
+                    TextInput::make('slug')->required()
+                ])
             ]);
     }
 
@@ -30,7 +39,9 @@ class TagsRelationManager extends RelationManager
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('name'),
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->limit(50)->sortable(),
+                TextColumn::make('slug')->limit(50)
             ])
             ->filters([
                 //
@@ -48,5 +59,5 @@ class TagsRelationManager extends RelationManager
                 Tables\Actions\DetachBulkAction::make(),
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
-    }    
+    }
 }
