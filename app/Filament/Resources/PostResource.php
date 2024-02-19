7c78b575432f69056fde96c7454bb66e5fae427b
Ali <qali372@gmail.com>
2024-02-19 07:14:39 +0200

    feat : add picture to Posts and table in PostResource.php

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -10,12 +10,16 @@ use Filament\Forms;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Str;
@@ -34,12 +38,13 @@ class PostResource extends Resource
                     Select::make('category_id')
                         ->relationship('category', 'name'),
                     TextInput::make('title')->reactive()
-                        ->afterStateUpdated(function (Closure $set,$state){
+                        ->afterStateUpdated(function (Closure $set, $state) {
                             $set('slug', Str::slug($state));
                         })->required(),
                     TextInput::make('slug')->required(),
+                    SpatieMediaLibraryFileUpload::make('thumbnail')->collection('posts'),
                     RichEditor::make('content'),
-                    Toggle::make('is_published')
+                    Toggle::make('is_published'),
                 ])
             ]);
     }
@@ -48,7 +53,11 @@ class PostResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('title')->limit(50)->sortable(),
+                TextColumn::make('slug')->limit(50),
+                BooleanColumn::make('is_published'),
+                SpatieMediaLibraryImageColumn::make('thumbnail')->collection('posts'),
             ])
             ->filters([
                 //
