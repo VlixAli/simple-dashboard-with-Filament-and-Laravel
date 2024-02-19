57616f562af4680345d48965855522726896fdfc
Ali <qali372@gmail.com>
2024-02-19 07:51:51 +0200

    feat : add PostsRelationManager in getRelations in TagResource.php

--- app/Filament/Resources/TagResource.php
+++ app/Filament/Resources/TagResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\TagResource\Pages;
 use App\Filament\Resources\TagResource\RelationManagers;
+use App\Filament\Resources\TagResource\RelationManagers\PostsRelationManager;
 use App\Models\Tag;
 use Closure;
 use Filament\Forms;
@@ -60,7 +61,7 @@ class TagResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            PostsRelationManager::class
         ];
     }
 
